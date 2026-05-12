2268ffed35c1282a02ee7b4fac16f6dc43489e1e
Ujjwal Singh <160165035+fallofpheonix@users.noreply.github.com>
2026-05-12 16:05:15 +0530

    Sprint 1: Domain Model Weight

---
+++ lib/domain/models/weight.dart
@@ -0,0 +1,13 @@
+class Weight {
+      final String id;
+        final DateTime timestamp;
+          final double valueKg;
+
+            const Weight({
+                required this.id,
+                    required this.timestamp,
+                        required this.valueKg,
+                          });
+                          }
+                          
+}
